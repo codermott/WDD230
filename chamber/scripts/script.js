41f4a2f7cac916af37134d808c8c3eff7b950362
codemrott <ode16003@byui.edu>
2023-05-14 16:07:47 -0600

    Commewrce update

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,9 +1,9 @@
-const hamburger = document.querySelector(".hamburger");
-const menu = document.querySelector(".menu");
-
-hamburger.addEventListener("click", () => {
-  menu.classList.toggle("menu-open");
-});
+function toggleMenu(){
+  document.getElementById("primarynav").classList.toggle("open");
+}
+const x = document.getElementById('hamburgerbtn')
+x.onclick = toggleMenu;
+ 
 
 var lastModified = new Date(document.lastModified);
 document.getElementById("last-modified").innerHTML = "Last modified: " + lastModified.toLocaleString();
