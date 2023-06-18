5213e53efd4edd2c40487ea8a24ac3746866cfe1
codemrott <ode16003@byui.edu>
2023-06-17 18:25:16 -0600

    Missing pages in development

---
+++ discoverpage/discovercript.js
@@ -0,0 +1,16 @@
+function toggleMenu(){
+    document.getElementById("primarynav").classList.toggle("open");
+  }
+  const x = document.getElementById('hamburgerbtn')
+  x.onclick = toggleMenu;
+  
+  const currentDate = new Date();
+  const currentTime = currentDate.toLocaleTimeString();
+  
+  const dateContainer = document.getElementById("current-date");
+  const timeContainer = document.getElementById("current-time");
+  dateContainer.textContent = currentDate.toDateString();
+  timeContainer.textContent = currentTime;
+  
+  var lastModified = new Date(document.lastModified);
+  document.getElementById("last-modified").innerHTML = "Last modified: " + lastModified.toLocaleString();
